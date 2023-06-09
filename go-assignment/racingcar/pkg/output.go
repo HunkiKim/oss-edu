c854315fdfdd6ee0c96799634d162ffc93a9f744
HunkiKim <hgkim@mantech.co.kr>
2023-06-09 11:24:35 +0900

    Racing 과제 수행 요구사항 1
    * CLI를 통한 이름, 도는 횟수 입력 및 Validation
    
    * Racing 로직 작성
    
    * 유닛 테스트 작성
    
    * 출력 포맷에 맞춰 CLI를 통한 결과값 출력
    
    approve by 조대식

---
+++ go-assignment/racingcar/pkg/output.go
@@ -0,0 +1,49 @@
+package pkg
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+const (
+	printingNameLength = 5
+	maxTopUsersNum     = 3
+)
+
+func PrintRank(users []*user) {
+	sortUsers(users)
+
+	min := func(x, y int) int {
+		if x > y {
+			return y
+		}
+		return x
+	}
+	topUsers := users[:min(maxTopUsersNum, len(users))]
+	for _, user := range topUsers {
+		fmt.Printf("%s:%s\n", parseNameByLength(user), strings.Repeat("-", user.numberOfTurns))
+	}
+}
+
+func sortUsers(users []*user) {
+	sort.Slice(users, func(i, j int) bool {
+		switch {
+		case users[i].numberOfTurns > users[j].numberOfTurns:
+			return true
+		case users[i].numberOfTurns < users[j].numberOfTurns:
+			return false
+		case users[i].name < users[j].name:
+			return true
+		default:
+			return false
+		}
+	})
+}
+
+func parseNameByLength(user *user) string {
+	if len(user.name) < printingNameLength+1 {
+		return user.name + strings.Repeat(" ", printingNameLength-len(user.name))
+	}
+	return user.name[:printingNameLength]
+}
